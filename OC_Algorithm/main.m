5a94944c74bb93cc66a62b9df96e1079ecd8e09b
YunTu <yuntu@YunTudeMac-mini.local>
2015-08-31 10:15:05 +0800

    Initial Commit

---
+++ OC_Algorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_Algorithm
+//
+//  Created by YunTu on 15/8/31.
+//  Copyright (c) 2015年 YunTu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
